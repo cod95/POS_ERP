30526c074d2b3b73c2f03eef8c66e360cdfe056f
cod95 <148455928+cod95@users.noreply.github.com>
2026-09-22 23:41:38 +0300

    Place payment mapping helper at class scope

--- POS/ViewModels/POSViewModel.cs
+++ POS/ViewModels/POSViewModel.cs
@@ -138,6 +138,16 @@ namespace POS.ViewModels
         public ICommand DeliveryCommand { get; }
         public ICommand SuspendBillCommand { get; }
         public ICommand CancelBillCommand { get; }
+        private POS.Domain.Models.Payments.PaymentType MapPaymentType(string paymentMethod)
+        {
+            return paymentMethod switch
+            {
+                "كارت" => POS.Domain.Models.Payments.PaymentType.CreditCard,
+                "على الحساب" => POS.Domain.Models.Payments.PaymentType.OnAccount,
+                _ => POS.Domain.Models.Payments.PaymentType.Cash
+            };
+        }
+
         public POSViewModel() : base()
         {
             BillNumber = GetNextInvoiceNumber(false);
@@ -151,16 +161,6 @@ namespace POS.ViewModels
             DeliveryCommand = new RelayCommand(ExecuteDelivery);
             SuspendBillCommand = new RelayCommand(ExecuteSuspendBillCommand);
             CancelBillCommand = new RelayCommand(ExecuteCancelBill);
-        private POS.Domain.Models.Payments.PaymentType MapPaymentType(string paymentMethod)
-        {
-            return paymentMethod switch
-            {
-                "كارت" => POS.Domain.Models.Payments.PaymentType.CreditCard,
-                "على الحساب" => POS.Domain.Models.Payments.PaymentType.OnAccount,
-                _ => POS.Domain.Models.Payments.PaymentType.Cash
-            };
-        }
-
         #region CartListEvents
             CartList_CurrentCellChangedCommand = new RelayCommand(ExecuteCartList_CurrentCellChangedCommand);
             CartList_SelectionChangedCommand = new RelayCommand(ExecuteCartList_SelectionChangedCommand);
